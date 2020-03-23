6032cb60e4c2297b9c45ce4811e48f531a17cb71
hhq163 <hhq163@gmail.com>
2020-03-23 20:10:04 +0800

    add test case

--- example/main.go
+++ example/main.go
@@ -1,5 +1,61 @@
 package main
 
-func main(){
-	
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"time"
+
+	"github.com/gpmgo/gopm/log"
+	"github.com/hhq163/rabbitmq_channel_pool/impl"
+	"github.com/streadway/amqp"
+)
+
+type PlayerData struct {
+	Uid       int32
+	UserName  string
+	AgentName string
+}
+
+func main() {
+	channelPool := new(impl.ChannelPool)
+	channelPool.InitPool("amqp://test:testpassword@192.168.31.230:5672/test")
+
+	userData := &PlayerData{
+		Uid:       1,
+		UserName:  "testUser",
+		AgentName: "baoshan",
+	}
+	start := time.Now().UnixNano()
+
+	sendCount := 0
+	for i := 1; i < 10000; i++ {
+		userData.Uid = i
+
+		datas := new(bytes.Buffer)
+		enc := gob.NewEncoder(datas)
+		err := enc.Encode(userData)
+		if err != nil {
+			log.Error("Encode error")
+			continue
+		}
+		msg := amqp.Publishing{
+			Headers:         amqp.Table{},
+			ContentType:     "text/plain",
+			ContentEncoding: "",
+			Body:            datas.Bytes(),
+			DeliveryMode:    amqp.Transient, // 1=non-persistent, 2=persistent
+			Priority:        0,              // 0-9
+		}
+
+		errs := channelPool.Publish("TestExchange", "testKey", false, false, false, msg)
+		if errs != nil {
+			log.Error("Failed to publish a message grid:", tf.grid, "errinfo:", err.Error())
+		}
+		sendCount++
+	}
+	dur := time.Now().UnixNano() - start
+	speed := 1000 / dur
+	fmt.Println("Publish speed is", speed)
+
 }
