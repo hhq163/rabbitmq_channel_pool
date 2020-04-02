6f07032f6cde345a8c1290479f54ddde2c412496
hhq163 <hhq163@gmail.com>
2020-04-02 21:36:38 +0800

    optimize

--- example/main.go
+++ example/main.go
@@ -6,9 +6,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"time"
 
-	br "github.com/hhq163/breaker"
 	"github.com/hhq163/rabbitmq_channel_pool/base"
 	"github.com/hhq163/rabbitmq_channel_pool/impl"
 	"github.com/hhq163/rabbitmq_channel_pool/util"
@@ -30,17 +28,8 @@ type PlayerData struct {
 
 func main() {
 	base.LogInit("Info", 1000)
-	options := br.Options{
-		BucketTime:        150 * time.Millisecond,
-		BucketNums:        6450, //每秒4万次请求，超过这个值熔断
-		BreakerRate:       0.3,  //错误率阀值
-		BreakerMinSamples: 300,
-		CoolingTimeout:    3 * time.Second,        //冷却时间，打开后，过冷却时间后变成半打开
-		DetectTimeout:     150 * time.Millisecond, //检测时间，半打开状态以检测时间去发送请求，成功次数到达HalfOpenSuccess后，关闭熔断器
-		HalfOpenSuccess:   3,
-	}
-	breakers := br.InitBreakers([]int32{1000}, options)
-	cpBreaker := breakers.GetBreaker(1000)
+	base.BreakerInit()
+	cpBreaker := base.GetBreaker(1000)
 
 	channelPool := new(impl.ChannelPool)
 	channelPool.InitPool("amqp://admin:2626hhq@192.168.1.29:5672/")
