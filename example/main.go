0ea68c5af92b27f6202a306578f2712c142ca4b2
hhq163 <hhq163@gmail.com>
2020-04-02 21:27:08 +0800

    fix bug

--- example/main.go
+++ example/main.go
@@ -8,6 +8,7 @@ import (
 	"os/signal"
 	"time"
 
+	br "github.com/hhq163/breaker"
 	"github.com/hhq163/rabbitmq_channel_pool/base"
 	"github.com/hhq163/rabbitmq_channel_pool/impl"
 	"github.com/hhq163/rabbitmq_channel_pool/util"
@@ -29,8 +30,7 @@ type PlayerData struct {
 
 func main() {
 	base.LogInit("Info", 1000)
-
-	options := breaker.Options{
+	options := br.Options{
 		BucketTime:        150 * time.Millisecond,
 		BucketNums:        6450, //每秒4万次请求，超过这个值熔断
 		BreakerRate:       0.3,  //错误率阀值
@@ -39,8 +39,8 @@ func main() {
 		DetectTimeout:     150 * time.Millisecond, //检测时间，半打开状态以检测时间去发送请求，成功次数到达HalfOpenSuccess后，关闭熔断器
 		HalfOpenSuccess:   3,
 	}
-	breakers := breaker.InitBreakers([]int32{1000}, options)
-	cpBreaker = breakers.GetBreaker(1000)
+	breakers := br.InitBreakers([]int32{1000}, options)
+	cpBreaker := breakers.GetBreaker(1000)
 
 	channelPool := new(impl.ChannelPool)
 	channelPool.InitPool("amqp://admin:2626hhq@192.168.1.29:5672/")
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/hhq163/rabbitmq_channel_pool
 go 1.13
 
 require (
-	github.com/hhq163/breaker v0.0.0-20190101075239-dd628667cdba
+	github.com/hhq163/breaker v0.0.0-20200402094822-9a1024f32dc2
 	github.com/streadway/amqp v0.0.0-20200108173154-1c71cc93ed71
 )
